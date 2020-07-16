ff5d8615cf3fc245d3f966f90f2c5c32137ab55e
Weidong Meng <>
2020-07-16 22:39:58 +0800

    testtest

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  BusinessTribe
+//
+//  Created by Weidong Meng on 2020/7/16.
+//  Copyright © 2020 mwd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  BusinessTribe
+//
+//  Created by Weidong Meng on 2020/7/16.
+//  Copyright © 2020 mwd. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
